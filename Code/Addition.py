1d8ccdaa76d8f965c999553c45a065ce8c7bcec2
ftumpa001 <ftump001@ucr.edu>
2026-02-23 17:06:38 -0800

    Addition.py

--- Code/Addition.py
+++ Code/Addition.py
@@ -135,7 +135,8 @@ def dd_MLCS_addition(strings: list[str]):
 
 # -------------------------------------------------------------
 def load_sequences_from_file(path: str) -> list[str]:
-    with open(path) as f:
+    #with open(path) as f:
+    with open(path, "r", encoding="latin-1") as f:
         return [line.strip() for line in f if line.strip()]
 
 # -------------------------------------------------------------
